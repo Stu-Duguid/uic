b0118dfc8844f16c03b4b29dfcc9496e5a1acd30
Stu-Duguid <56097530+Stu-Duguid@users.noreply.github.com>
2023-05-08 13:56:30 +1000

    fix integration approach

--- modules/ab testing/abtasty.js
+++ modules/ab testing/abtasty.js
@@ -1,28 +1,46 @@
 
 // ABTasty integration
-function sendABTastyData() {
-    window.TLT.abtTries = window.TLT.abtTries || 0;
-    window.TLT.abtTries++;
-    // console.debug(`tealeaf abtasty: try ${window.TLT.abtTries}`)
-    if ('ABTasty' in window && window.ABTasty.getTestsOnPage && typeof window.ABTasty.getTestsOnPage === 'function') {
-        // console.debug('tealeaf abtasty: found');
-        var abTastyTests = window.ABTasty.getTestsOnPage();
-        for (var abTasteTest in abTastyTests) {
-            // console.debug('tealeaf abtasty: ', abTasteTest);
-            var abExp = abTastyTests[abTasteTest];
-            // console.debug('tealeaf abtasty: ', abTasteTest, abExp.name, abExp.variationID, abExp.variationName);
-            window.TLT.logCustomEvent("abTest", {
-                description: "ABTasty",
-                experimentId: abTasteTest,
-                experiment: abExp.name,
-                variantId: abExp.variationID,
-                variant: abExp.variationName
-            });
-            // console.debug('tealeaf abtasty: ', abTasteTest, abExp.name, abExp.variationID, abExp.variationName);
-        }
-    } else {
-        if (window.TLT.abtTries < 40)
-            setTimeout(sendABTastyData, 500);
+// function sendABTastyData() {
+//     window.TLT.abtTries = window.TLT.abtTries || 0;
+//     window.TLT.abtTries++;
+//     // console.debug(`tealeaf abtasty: try ${window.TLT.abtTries}`)
+//     if ('ABTasty' in window && window.ABTasty.getTestsOnPage && typeof window.ABTasty.getTestsOnPage === 'function') {
+//         // console.debug('tealeaf abtasty: found');
+//         var abTastyTests = window.ABTasty.getTestsOnPage();
+//         for (var abTasteTest in abTastyTests) {
+//             // console.debug('tealeaf abtasty: ', abTasteTest);
+//             var abExp = abTastyTests[abTasteTest];
+//             // console.debug('tealeaf abtasty: ', abTasteTest, abExp.name, abExp.variationID, abExp.variationName);
+//             window.TLT.logCustomEvent("abTest", {
+//                 description: "ABTasty",
+//                 experimentId: abTasteTest,
+//                 experiment: abExp.name,
+//                 variantId: abExp.variationID,
+//                 variant: abExp.variationName
+//             });
+//             // console.debug('tealeaf abtasty: ', abTasteTest, abExp.name, abExp.variationID, abExp.variationName);
+//         }
+//     } else {
+//         if (window.TLT.abtTries < 40)
+//             setTimeout(sendABTastyData, 500);
+//     }
+// }
+// setTimeout(sendABTastyData, 4000);
+
+// version to insert into AB Tasty integrator
+
+function sendTealeafData() {
+    if (window.TLT && window.TLT.isInitialized()) {
+        window.TLT.logCustomEvent("abTest", {
+            description: "ABTasty",
+            experimentId: {{ campaignId }},
+            experiment: {{ campaignName }},
+            variantId: {{ variationId }},
+            variant: {{ variationName }}
+        });
+    }
+    else {
+      setTimeout(sendTealeafData, 200);
     }
 }
-setTimeout(sendABTastyData, 4000);
+sendTealeafData();
