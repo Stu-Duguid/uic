0f079d848885e5a30de178e381eb6295684ecbfd
Stu-Duguid <56097530+Stu-Duguid@users.noreply.github.com>
2024-06-05 09:45:19 +1000

    simpler

--- modules/only on page event.js
+++ modules/only on page event.js
@@ -1,3 +1,4 @@
+function E_EVENTNAME() {
 {
     // change storage text, pattern name, and event name
     const store = "text_xxx";
@@ -7,6 +8,20 @@
     {
         prev.timestamp = $S.LastHitEpoch;
         $S.setCustomJSObj(store, prev);
-        $F.setFacts("E_TEXT_XXX");
+        $F.setFacts("E_EVENTNAME");
+    }
+}
+
+
+// tested using lookup on the last instance of itself
+
+function E_EVENTNAME() {
+    if ($P["P_PATTERN"].patternFound()) {
+        var load = $F.getLastHitTime("TL.E_ALL_PAGE_LOADS");
+        var prev = $F.getLastHitTime("E_EVENTNAME");
+
+        if (prev < load) {
+            $F.setFacts("E_EVENTNAME");
+        }
     }
 }
