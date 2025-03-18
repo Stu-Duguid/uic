24a0ba8d4987f73cc123146fc86539146516b6d8
Stu-Duguid <56097530+Stu-Duguid@users.noreply.github.com>
2025-03-18 15:11:36 +1100

    2025.03a iframes

--- my.template.js
+++ my.template.js
@@ -11,10 +11,17 @@
 
     var config = TLT.getDefaultConfig();
 
-    config.core.buildNote = "template 2024.12a";
+    config.core.buildNote = "template 2025.03a";
     config.core.sessionDataEnabled = true;
 
     config.core.modules.dataLayer.enabled = false;
+
+    // Enable for cross-domain communication
+    // config.core.frames.enableCrossDomainCommunication = true
+    // In child page, indicate the ID of the iframe to be used on parent page.
+    // if (document.location.hostname.indexOf("frame_domain") != -1) {
+    //     config.core.frames.eventProducer.producerId = "id_of_iframe_in_parent";
+    // }
 
     config.core.modules.frictionSigns = {
         events: [
@@ -104,25 +111,35 @@
     config.services.message.privacyPatterns = [
         {
             // any email
-        	pattern: { regex: /[\w\+\-]+@(?:[\w\+\-]+\.)+[\w\+\-]{2,}/, flags: "g" },
-            replacement: function (match) {
-                return maskXx9(match);
-            }
+            pattern: { regex: /[\w\+\-]+@(?:[\w\+\-]+\.)+[\w]{2,}/, flags: "g" },
+            replacement: function (match) { return match.replace(/[A-Z]/g, "X").replace(/[a-z]/g, "x").replace(/[0-9]/g, "9") }
         },
         {
-            // any phone
-        	pattern: { regex: /\d\d[\d ]{6,10}/, flags: "g" },
-            replacement: function (match) {
-                return maskXx9(match);
-            }
+            // any phone starting with 0
+            pattern: { regex: /(\D)(0[2-9][\d ]{6,10})/, flags: "g" },
+            replacement: function (match, g1, g2) { return g1 + g2.replace(/[0-9]/g, "9"); }
         },
         {
-            // any date
-        	pattern: { regex: /\d\d?\/\d\d?\/\d\d(\d\d)?/, flags: "g" },
-            replacement: function (match) {
-                return maskXx9(match);
-            }
-        }
+            // any phone starting with 8 or 9 e.g. 9876 4534
+            pattern: {
+                regex: /[89]\d\d\d \d\d\d\d/,
+                flags: "g"
+            },
+            replacement: function (match) { return match.replace(/[0-9]/g, "9") }
+        },
+        {
+            // any date as 99/99/99, 9/9/9999
+            pattern: { regex: /\d\d?\/\d\d?\/\d\d(\d\d)?/, flags: "g" },
+            replacement: function (match) { return match.replace(/[0-9]/g, "9") }
+        },
+        {
+            // remove dates 4 Dec 1988
+            pattern: {
+                regex: /\d\d? [JFMASOND][aepuco][nbrylgptvc],? \d\d(\d\d)?/,
+                flags: "g"
+            },
+            replacement: function (match) { return match.replace(/[A-Z]/g, "X").replace(/[a-z]/g, "x").replace(/[0-9]/g, "9") }
+        },
     ];
 
     // config.modules.ajaxListener.urlBlocklist.push(
