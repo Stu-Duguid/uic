82c3f188f3a538d1f6853efc761e6e2fc1be629a
Stu-Duguid <56097530+Stu-Duguid@users.noreply.github.com>
2025-05-04 09:57:08 +1000

    reorg

--- my.template.js
+++ my.template.js
@@ -185,6 +185,48 @@
     //     { event: "load", delayUntil: { selector: "div.page-preloader[style='display: none;']", exists: false, timeout: 2000 } },
     // ];
 
+    // TLT.registerBridgeCallbacks([{
+    //     enabled: true,
+    //     cbType: "messageRedirect",
+    //     cbFunction: function (_msg, msgObj) {
+    //         if (msgObj) {
+    //             if (!msgObj.screenviewDone) {
+    //                 msgObj.screenviewDone = true; // to prevent endless loop
+                    
+    //                 /**
+    //                  * Rename the properties that cannot be addressed with dot notation and require [] notation
+    //                  * Also turn arrays into strings
+    //                  * @param {Object} object the object to be cleaned
+    //                  * @returns {Object} a copy of the object after cleaning
+    //                 */
+    //                function cleanObject(object) {
+    //                    if (typeof object !== "object") {
+    //                        return object;
+    //                     }
+    //                     // if an array, convert to a string
+    //                     if (Array.isArray(object)) {
+    //                         return JSON.stringify(object);
+    //                     }
+    //                     // operate on a copy to not alter original
+    //                     var prop, copy = {};
+    //                     // object so iterate through properties and recursively clean them of objects
+    //                     for (prop in object) {
+    //                         copy[prop.replace(/(\.|-)/g, '_').replace(/^(\d)/, '_$1')] = cleanObject(object[prop]);
+    //                     }
+    //                     return copy;
+    //                 }
+    //                 if (msgObj.type === 5) {
+    //                     msgObj.customEvent.data = cleanObject(msgObj.customEvent.data);
+    //                 }
+    //                 if (msgObj.type === 19) {
+    //                     msgObj.dataLayer = cleanObject(msgObj.dataLayer);
+    //                 }
+    //             }
+    //         }
+    //         return msgObj;
+    //     }
+    // }]);
+
     var appKey = "2b5f323f11804851beb8617eee293042";
     if (window.location.hostname === "www.prod.com" || window.location.hostname === "other.prod.com") {
         appKey = "prod appkey value"; // production
