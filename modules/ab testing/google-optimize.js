5dc3dce28177103ac6fdc1f3ea450c6eb128a088
Stu-Duguid <56097530+Stu-Duguid@users.noreply.github.com>
2025-05-04 10:03:26 +1000

    reorg

--- modules/ab testing/google-optimize.js
+++
@@ -1,9 +0,0 @@
-
-// google optimize tealeaf integration
-
-if (window.google_optimize) {
-	window.TLT.logGOptimize = function (val, name) {
-		window.TLT.logCustomEvent("optimize", {description: "Optimize", experiment: name, variant: val});
-	}
-	(function () {window.dataLayer.push(arguments)}('event', 'optimize.callback', {callback: window.TLT.logGOptimize}));
-}
--- modules/devplugin/README.md
+++
@@ -1,3 +0,0 @@
-# Tealeaf
-
-some text needed
